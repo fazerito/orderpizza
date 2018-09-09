316532075a83b0e8bcbc766553b2924bddaede2d
Marcin <gervvaz@gmail.com>
2018-09-09 22:14:13 +0200

    Added urls.py

---
+++ orders/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("menu/", views.menu, name="menu"),
+    path("about/", views.about, name="about"),
+    path("register/", views.register, name="register")
+]
