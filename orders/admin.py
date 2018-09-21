c4f5c6aa11bbeb68d7162af643987fb0a22ff97e
Marcin <gervvaz@gmail.com>
2018-09-21 18:15:05 +0200

    Renamed user model

--- orders/admin.py
+++ orders/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Pizza, Topping, User
+from .models import Pizza, Topping, UserProfile
 from django.contrib.auth.admin import UserAdmin
 
 
-admin.site.register(User)
+admin.site.register(UserProfile)
 admin.site.register(Pizza)
 admin.site.register(Topping)
