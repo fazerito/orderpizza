9e1dc52ad550150e74934c6b60fef1151b97c9cc
Marcin <gervvaz@gmail.com>
2018-09-21 18:14:33 +0200

    Renamed user model

--- orders/models.py
+++ orders/models.py
@@ -2,13 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class User(models.Model):
+class UserProfile(models.Model):
     username = models.OneToOneField(User, on_delete=models.CASCADE)
+    email = models.EmailField(default='')
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=25)
-    address = models.CharField(max_length=1024)
-    city = models.CharField(max_length=20)
-    zip_code = models.CharField(max_length=6)
+
 
     def __str__(self):
         return f'{self.first_name}  {self.last_name}, {self.address} {self.city}, ZIP CODE: {self.zip_code}'
@@ -36,5 +35,5 @@ class Pizza(models.Model):
 
 class Order(models.Model):
     product_name = models.ForeignKey(Pizza, on_delete=models.CASCADE)
-    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    customer = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     total_cost = models.IntegerField()
