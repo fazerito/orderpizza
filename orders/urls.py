e839aa6f1719eacc8fc4856551f194524a23620c
Marcin <gervvaz@gmail.com>
2018-09-21 18:15:50 +0200

    Added login/logout urls

--- orders/urls.py
+++ orders/urls.py
@@ -1,10 +1,25 @@
+from django.conf.urls import url
 from django.urls import path
+from django.contrib.auth.views import (
+    login, logout, password_reset, password_reset_done, password_reset_confirm,
+    password_reset_complete
+)
 
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("menu/", views.menu, name="menu"),
-    path("about/", views.about, name="about"),
-    path("register/", views.register, name="register")
+    url(r'^$', views.index, name="index"),
+    url(r'^menu/$', views.menu, name="menu"),
+    url(r'^about/$', views.about, name="about"),
+    url(r'^register/$', views.register, name="register"),
+    url(r'^login/$', views.login_user, name="login"),
+    url(r'^logout/$', views.logout_user, name="logout"),
+    url(r'^reset-password/$', password_reset, name='reset_password'),
+    url(r'^reset-password/done/$', password_reset_done, name='password_reset_done'),
+    url(r'^reset-password/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,23})/$',
+        password_reset_confirm, name='password_reset_confirm'),
+    #url(r'^reset-password/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>.+)/$', password_reset_confirm, name='password_reset_confirm'),
+    url(r'^reset-password/complete/$', password_reset_complete, name='password_reset_complete'),
+
+
 ]
