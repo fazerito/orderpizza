4496cc04f1b9e0928347771fd38d793d705452a5
Marcin <gervvaz@gmail.com>
2018-10-29 18:35:09 +0100

    added order model

--- orders/models.py
+++ orders/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from django.contrib.auth.models import User
 
 # Create your models here.
@@ -10,30 +11,45 @@ class UserProfile(models.Model):
 
 
     def __str__(self):
-        return f'{self.first_name}  {self.last_name}, {self.address} {self.city}, ZIP CODE: {self.zip_code}'
+        return f'{self.first_name}  {self.last_name}'
 
 class Topping(models.Model):
     name = models.CharField(max_length=64)
-    price = models.IntegerField()
+    price = models.DecimalField(max_digits=2, decimal_places=1)
 
     def __str__(self):
         return f'{self.name}'
 
 class Pizza(models.Model):
     PIZZA_SIZES = (
-        ('s', 'Small'),
-        ('m', 'Medium'),
-        ('l', "Large")
+        ('small', 'Small'),
+        ('large', "Large")
     )
     name = models.CharField(max_length=64)
-    size = models.CharField(max_length=1, choices=PIZZA_SIZES)
+    slug = models.SlugField(max_length=200, default='')
+    size = models.CharField(max_length=5, choices=PIZZA_SIZES)
+    stock = models.PositiveIntegerField()
     toppings = models.ManyToManyField(Topping)
-    price = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         return f'{self.name}, size: {self.size}, toppings: {self.toppings.all()}'
 
+    def get_absolute_url(self):
+        return reverse('orders:pizza_detail',
+                       args=[self.id, self.slug])
+
 class Order(models.Model):
     product_name = models.ForeignKey(Pizza, on_delete=models.CASCADE)
     customer = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
-    total_cost = models.IntegerField()
+    address = models.CharField(max_length=150, default='')
+    postal_code = models.CharField(max_length=30, default='')
+    city = models.CharField(max_length=100, default='')
+    quantity = models.PositiveIntegerField(default=1)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default='99')
+
+    def __str__(self):
+        return f'{self.id}'
+
+    def get_cost(self):
+        return self.price * self.quantity
