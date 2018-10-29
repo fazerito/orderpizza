8606afffbb250df93360b7884905cd1b7ae482bf
Marcin <gervvaz@gmail.com>
2018-10-29 18:35:53 +0100

    updated files

--- orders/urls.py
+++ orders/urls.py
@@ -10,6 +10,9 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name="index"),
     url(r'^menu/$', views.menu, name="menu"),
+    url(r'^(?P<id>\d+)/(?P<slug>[-\w]+)/$',
+        views.pizza_detail,
+        name='pizza_detail'),
     url(r'^about/$', views.about, name="about"),
     url(r'^register/$', views.register, name="register"),
     url(r'^login/$', views.login_user, name="login"),
@@ -18,7 +21,8 @@ urlpatterns = [
     url(r'^reset-password/done/$', password_reset_done, name='password_reset_done'),
     url(r'^reset-password/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,23})/$',
         password_reset_confirm, name='password_reset_confirm'),
-    #url(r'^reset-password/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>.+)/$', password_reset_confirm, name='password_reset_confirm'),
+    #url(r'^reset-password/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>.+)/$', password_reset_confirm, 
+    #                                       name='password_reset_confirm'),
     url(r'^reset-password/complete/$', password_reset_complete, name='password_reset_complete'),
 
 
--- orders/views.py
+++ orders/views.py
@@ -1,16 +1,27 @@
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import RegistrationForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
+from .models import Pizza
+from cart.forms import CartAddPizzaForm
 
 def index(request):
     return render(request, "orders/index.html")
 
-@login_required
 def menu(request):
-    return render(request, "orders/menu.html")
+    pizzas = Pizza.objects.all()
+    return render(request, "orders/menu.html", {'pizzas': pizzas})
+
+def pizza_detail(request, id, slug):
+    pizza = get_object_or_404(Pizza, id=id, slug=slug)
+    cart_product_form = CartAddPizzaForm()
+    args = {
+        'pizza': pizza,
+        'cart_product_form': cart_product_form,
+    }
+    return render(request, 'orders/pizza_detail.html', args)
 
 def about(request):
     return render(request, "orders/about.html")
@@ -25,7 +36,7 @@ def register(request):
     else:
         form = RegistrationForm()
     args = {'form': form,
-                'registered': registered}
+            'registered': registered}
     return render(request, "orders/register_form.html", args)
 
 
