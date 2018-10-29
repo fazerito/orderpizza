c10300e931c86934591b9168eef2d41f827d4902
Marcin <gervvaz@gmail.com>
2018-10-29 18:34:21 +0100

    added admin file

--- orders/admin.py
+++ orders/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from .models import Pizza, Topping, UserProfile
-from django.contrib.auth.admin import UserAdmin
 
+
+class PizzaAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'price', 'stock']
+    list_editable = ['price', 'stock']
+    prepopulated_fields = {'slug': ('name', 'size')}
 
 admin.site.register(UserProfile)
-admin.site.register(Pizza)
 admin.site.register(Topping)
+admin.site.register(Pizza, PizzaAdmin)
